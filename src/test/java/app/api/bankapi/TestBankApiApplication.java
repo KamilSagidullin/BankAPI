f302d39e30fff260cf92f70ec713ed3158f9c212
kamilsagidullin <rf-20@inbox.ru>
2025-11-03 16:50:14 +0300

    Project structure added

---
+++ src/test/java/app/api/bankapi/TestBankApiApplication.java
@@ -0,0 +1,11 @@
+package app.api.bankapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BankApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
